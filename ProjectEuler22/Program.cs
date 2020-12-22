b508783839405d82c4e21ac9f4abf5f24f5861ed
ChuyueL <chuyueluo@gmail.com>
2020-12-22 02:53:14 +0000

    Added solution for problem 22

---
+++ ProjectEuler22/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+
+namespace ProjectEuler22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:\\Users\\chuyu\\Documents\\p022_names.txt"))
+                {
+                    char c;
+                    string name = "";
+                    while (sr.Peek() > -1)
+                    {
+                        c = (char)sr.Read();
+                        if (c == ',')
+                        {
+                            names.Add(name);
+                            name = "";
+                            
+                        } 
+                        else if (c != '"')
+                        {
+                            name = name + c;
+                        }
+                        
+                    }
+                    names.Add(name); //Add last name in file
+
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            names.Sort();
+
+            BigInteger total = 0;
+            
+
+            foreach (string name in names)
+            {
+                total += CalculateScore(name, names.IndexOf(name) + 1);
+            }
+
+            Console.WriteLine(total);
+            Console.ReadLine();
+            
+        }
+
+        static int CalculateScore(string name, int pos)
+        {
+            int score = 0;
+            foreach (char c in name)
+            {
+                int index = char.ToUpper(c) - 64; //Find position of the character in alphabet
+                score += index;
+            }
+            score *= pos;
+
+            return score;
+        }
+    }
+}
