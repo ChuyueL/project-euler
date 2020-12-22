5cae9426173873398df76005d102d766ef4cc84b
ChuyueL <chuyueluo@gmail.com>
2020-12-22 01:31:36 +0000

    Added solution for problem 25

---
+++ ProjectEuler25/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+namespace ProjectEuler25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateFirstFib());
+        }
+
+        static long CalculateFirstFib()
+        {
+            BigInteger fib2 = 1;
+            BigInteger fib1 = 1;
+            BigInteger fib = fib1 + fib2;
+            long counter = 3;
+            string fibString = fib.ToString();
+
+            while (fibString.Length < 1000)
+            {
+                counter++;
+                fib2 = fib1;
+                fib1 = fib;
+                fib = fib1 + fib2;
+                fibString = fib.ToString();
+                if (counter < 10)
+                {
+                    Console.WriteLine(fib + "," + counter);
+                }
+                
+            }
+
+            return counter;
+        }
+    }
+}
