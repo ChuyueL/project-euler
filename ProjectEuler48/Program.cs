3c7b6c11a9fadee55b2772473d18322e4021bf6e
ChuyueL <chuyueluo@gmail.com>
2021-02-01 23:01:25 +0000

    Added solution for problem 48

---
+++ ProjectEuler48/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+
+namespace ProjectEuler48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger sum = SelfPower(1000);
+            string sumString = sum.ToString();
+            Console.WriteLine(sumString.Substring(sumString.Length - 10));
+            Console.ReadLine();
+        }
+
+        static BigInteger SelfPower(int num)
+        {
+            BigInteger sum = 0;
+
+            for (int i = 1; i < num+1; i++)
+            {
+                sum += BigInteger.Pow(i, i);
+            }
+
+            return sum;
+        }
+
+    }
+}
